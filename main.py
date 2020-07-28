00a0f1aab93f5f225eed9f1f86614f7fc914f45b
Randy Doan <randydoan109@gmail.com>
2020-07-28 14:36:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+def open_file(filename):
+  
+  
+def write_file(filename, data):
+  
+  
+  
+##### BEGINNING OF PROGRAM
+#test your functions here
+#create a text file
+#open the text file
+#make changes to the file
+#write the changes to the text file
